59e87b33ea6a85505a0421b03c891f01ada6b47c
Russel Musasa <russelmusasa@gmail.com>
2023-04-08 15:45:40 +0100

    Angular Intro - String Interpolation

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,8 +2,15 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'courses',
-    template: `<h2>Courses</h2>`
+    template: `<h2>Courses</h2>
+               <h3>{{ title }}</h3>`
 })
 export class CoursesComponent {
+
+    title = "List of courses";
+
+    getTitle() {
+        return this.title;
+    }
 
 }
