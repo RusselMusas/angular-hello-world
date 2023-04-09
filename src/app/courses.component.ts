8a09dc2a5d071a215bd9c01d9a652f6876d57225
Russel Musasa <russelmusasa@gmail.com>
2023-04-09 19:30:40 +0100

    Angular - Adding bootstrap for styling

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,14 +5,14 @@ import { CoursesService } from "./services/courses.service";
     selector: 'courses',
     template: `<h2>Courses</h2>
                <h3>{{ title }}</h3>
-               <img [src]="imageUrl" alt="nature image">`
+               <button class="btn btn-primary">Submit</button>`
 })
 export class CoursesComponent {
 
     title = "List of courses";
     courses: string[] | undefined = undefined;
     imageUrl = "https://thumbs.dreamstime.com/b/panorama-de-for%C3%AAt-avec-des-rayons-de-lumi%C3%A8re-du-soleil-53826213.jpg";
-
+    
     constructor(private coursesService: CoursesService) {}
 
     getTitle() {
